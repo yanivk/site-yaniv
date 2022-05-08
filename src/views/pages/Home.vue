bd8c3efc836e8e9a6c800af7e393884245c2701a
yaniv koubbi <yanivkoubbi@gmail.com>
2022-05-08 21:03:22 +0300

    [FEATURE] add background color effect menu transition...

--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="home">
-    <img
-      alt="Vue logo"
-      src="../../assets/logo.png"
-    >
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div><menu-content /></div>
+    <div class="content">
+      coucou
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import MenuContent from '@/components/menu/MenuContent.vue'
 
 @Options({
   components: {
+    MenuContent,
     HelloWorld
   }
 })
